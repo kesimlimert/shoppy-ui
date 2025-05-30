e3cd51c04aa16268cb28c1396e36b48f73ae9c7b
Mert Kesimli <mert_cod@hotmail.com>
2025-05-30 17:11:38 +0300

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import { Container, CssBaseline } from "@mui/material";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AppRouterCacheProvider>
+          <CssBaseline />
+            <Container>
+              {children}
+            </Container>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
